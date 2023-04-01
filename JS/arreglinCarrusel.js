445972acdb42882fe97f37cb7440d7136d582cd2
AtsukaDeus <AtsukaDeus@users.noreply.github.com>
2023-04-01 19:22:03 -0300

    cuarta version del proyecto

--- JS/arreglinCarrusel.js
+++ JS/arreglinCarrusel.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 10000,
       adaptiveHeight: true,
       responsive: [
         {
