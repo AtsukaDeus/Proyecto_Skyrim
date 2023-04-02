53335db73659c12f4af128c883bb0deddba9a30f
AtsukaDeus <AtsukaDeus@users.noreply.github.com>
2023-04-02 00:00:44 -0400

    The last version

--- JS/diccionarioValidacion.js
+++ JS/diccionarioValidacion.js
@@ -18,6 +18,21 @@ $(document).ready(function() {
         window.location.href = "../Paginas/RestaurarSalud.html";
         return;
       }
+
+      else if (x === 'pocion de escarcha' || x === 'resistencia a la escarcha' || x === 'escarcha' || x === 'frio' || x === 'resistencia al frio'){
+
+        window.location.href = "../Paginas/ResistenciaEscarcha.html";
+        return;
+      }
+
+      else if (x === 'pocion de respiracion acuatica' || x === 'respiracion acuatica' || x === 'respirar bajo el agua' || x === 'no ahogarse' || x === 'respirar'){
+
+        window.location.href = "../Paginas/RespiracionAcuatica.html";
+        return;
+      }
+
+      
+
       else {
         // El valor no cumple ninguna opción, muestra un mensaje de error
         alert('El valor ingresado no cumple ninguna opción válida.');
--- README.txt
+++ README.txt
@@ -3,3 +3,6 @@ Integrantes:
 Francisco Argandoña
 Renzo Torres
 
+Link Github:
+
+https://github.com/AtsukaDeus/Proyecto_Skyrim
