59d171402068b764c4951837fee896af18e4caff
AtsukaDeus <AtsukaDeus@users.noreply.github.com>
2023-05-06 20:37:55 -0400

    Añadiendo más paginas y mejorando formulario

--- JS/diccionarioValidacion.js
+++ JS/diccionarioValidacion.js
@@ -1,42 +1,48 @@
-$(document).ready(function() {
-    $('#principalForm').submit(function(event) {
-      // Prevenir que el formulario se envíe sin validar
-      event.preventDefault();
-  
-      // Obtener los valores de los campos del formulario
-      var x = $('#search').val().toLowerCase();
+  $(document).ready(function() {
+    
+    $('#principalForm').on('submit', function(event) {
+      event.preventDefault(); // Evita que se envíe el formulario de forma automática
+      var valor = $('input[name="buscar"]').val(); // Obtiene el valor del campo de texto
+      if (valor !== undefined) {
+        valor = valor.toLowerCase(); 
+      }
+      var coincidencia;
+
+      $.get('../pocionesCuracion.txt', function(contenido) { // Lee el contenido del archivo de texto plano
+          coincidencia = contenido.match(new RegExp(valor)); // Busca las coincidencias
+          
+          if (coincidencia !== null) {
+            window.location.href = "../Paginas/RestaurarSalud.html";
+            return;
+          } 
+
+      });
+
+      $.get('../pocionesRespiracionAcuatica.txt', function(contenido) { 
+        coincidencia = contenido.match(new RegExp(valor)); 
         
-      // Validar el campo nombre de pociones
-      if (x.length < 1) {
-        alert('Por favor, ingrese una busqueda.');
-        return;
-      }
-      
-      // opciones de pociones de curacion
-      else if (x === 'pocion de curacion' || x === 'curacion' || x === 'pocion de salud' || x === 'salud' || x === 'curarse'){
+        if (coincidencia !== null) {
+          window.location.href = "../Paginas/RespiracionAcuatica.html";
+          return;
+        } 
 
-        window.location.href = "../Paginas/RestaurarSalud.html";
-        return;
-      }
+      });
 
-      else if (x === 'pocion de escarcha' || x === 'resistencia a la escarcha' || x === 'escarcha' || x === 'frio' || x === 'resistencia al frio'){
+      $.get('../pocionesResistenciaEscarcha.txt', function(contenido) { 
+        coincidencia = contenido.match(new RegExp(valor)); 
+        
+        if (coincidencia !== null) {
+          window.location.href = "../Paginas/ResistenciaEscarcha.html";
+          return;
+        } 
 
-        window.location.href = "../Paginas/ResistenciaEscarcha.html";
-        return;
-      }
+      });
 
-      else if (x === 'pocion de respiracion acuatica' || x === 'respiracion acuatica' || x === 'respirar bajo el agua' || x === 'no ahogarse' || x === 'respirar'){
-
-        window.location.href = "../Paginas/RespiracionAcuatica.html";
-        return;
+      if(coincidencia === null){
+        alert("No se han encontrado coincidencias, intente con otras palabras.");
       }
 
-      
-
-      else {
-        // El valor no cumple ninguna opción, muestra un mensaje de error
-        alert('El valor ingresado no cumple ninguna opción válida.');
-      }
 
     });
   });
+  
